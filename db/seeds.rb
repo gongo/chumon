57333f30525d528f4dd98a03814ed812da2cf7c9
Wataru MIYAGUNI <gonngo@gmail.com>
2014-04-29 19:59:41 +0900

    テストデータを用意

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
+# 商品のテストデータ
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+('A'..'Z').each do |name|
+  Product.create(name: "商品#{name}", price: [100, 200, 300, 400].sample)
+end
+
+#
+# 注文明細のテストデータ
+#
+products = Product.all.to_a
+100.times do
+  OrderDetail.create(product: products.sample, count: (1..10).to_a.sample)
+end
