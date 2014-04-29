ccc508b1c4a99efcab864c53a791118ae125bf8b
Wataru MIYAGUNI <gonngo@gmail.com>
2014-04-29 15:59:57 +0900

    Start chumon project
        $ gem install rails -v '4.0.4'
        $ rails -v
        Rails 4.0.4
    
        $ rails new chumon
        $ cd chumon
        $ git init
        $ git add .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chumon::Application.config.secret_key_base = 'ea3aa9c37a65c4d9a37c1c056c0ba39337ad088cf47a73fb56b20d81a9c75071c22d3d2b3d71de03de5bbd81431fae49a14df44f869a91ea4b18e4989217dddb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chumon::Application.config.session_store :cookie_store, key: '_chumon_session'
