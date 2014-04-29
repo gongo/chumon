6c6e0657c2ed12e3778db867f12207598d7450cb
Wataru MIYAGUNI <gonngo@gmail.com>
2014-04-29 20:15:44 +0900

    多対多のチェック用テストデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,14 @@
   Product.create(name: "商品#{name}", price: [100, 200, 300, 400].sample)
 end
 
-#
-# 注文明細のテストデータ
-#
-products = Product.all.to_a
-100.times do
-  OrderDetail.create(product: products.sample, count: (1..10).to_a.sample)
+30.times do
+  products = Product.all.shuffle
+  order = Order.new
+
+  # 注文一つにつき3-5個のつまり商品を登録
+  (3..5).to_a.sample.times do
+    order.order_details.build(product: products.shift, count: (1..4).to_a.sample)
+  end
+
+  order.save
 end
