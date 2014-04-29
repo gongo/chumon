0df268a96c4cd6f85b4476b307b15f1ffe3619f6
Wataru MIYAGUNI <gonngo@gmail.com>
2014-04-29 19:31:45 +0900

    商品モデルにバリデーションを追加(演習用)
        - products.name に「2文字100文字以内」という条件を付けよ
        - products.price に「100以上の 数値」という条件を付けよ

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@
 
 class Product < ActiveRecord::Base
   validates :name, :price, presence: true # name と price は存在すべき
-  validates :name, length: { minimum: 2 } # name は最低でも二文字以上
+  validates :name, length: { in: 2..100 } # name は2文字以上100文字以内
+  validates :price, numericality: { greater_than_or_equal_to: 100 } # 100以上の数値
 end
