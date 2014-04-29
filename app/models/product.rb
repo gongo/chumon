66bdf8d2a1584b3c329fe47383e6141f5feae2fb
Wataru MIYAGUNI <gonngo@gmail.com>
2014-04-29 20:00:51 +0900

    演習の答えメソッド
      Product#sales_count   => 注文明細から、特定の商品の売上個数を取得する
      Product.sales_ranking => 商品の売上ランキング

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,16 @@ class Product < ActiveRecord::Base
   validates :price, numericality: { greater_than_or_equal_to: 100 } # 100以上の数値
 
   has_many :order_details
+
+  def self.sales_ranking(top = 10)
+    self
+      .joins(:order_details)
+      .group('order_details.product_id')
+      .order('SUM(order_details.count) DESC', :created_at)
+      .limit(top)
+  end
+
+  def sales_count
+    order_details.sum(:count)
+  end
 end
