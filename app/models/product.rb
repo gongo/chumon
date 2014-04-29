a1c76c56597df0c0abfa8ca6ea2e8b113532f446
Wataru MIYAGUNI <gonngo@gmail.com>
2014-04-29 19:28:05 +0900

    商品モデルにバリデーションを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
+# -*- coding: utf-8 -*-
+
 class Product < ActiveRecord::Base
+  validates :name, :price, presence: true # name と price は存在すべき
+  validates :name, length: { minimum: 2 } # name は最低でも二文字以上
 end
