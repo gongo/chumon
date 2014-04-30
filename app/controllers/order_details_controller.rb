a161d34da4a34247a340d7db9fa45f3cb05a0170
Wataru MIYAGUNI <gonngo@gmail.com>
2014-04-30 14:47:59 +0900

    共通処理を before_action として抜き出した

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -1,10 +1,11 @@
 class OrderDetailsController < ApplicationController
+  before_action :set_detail, only: [:show, :edit, :update, :destroy]
+
   def index
     @order_details = OrderDetail.all
   end
 
   def show
-    @detail = OrderDetail.find(params[:id])
   end
 
   def new
@@ -22,12 +23,9 @@ class OrderDetailsController < ApplicationController
   end
 
   def edit
-    @detail = OrderDetail.find(params[:id])
   end
 
   def update
-    @detail = OrderDetail.find(params[:id])
-
     if @detail.update(order_details_params)
       redirect_to @detail
     else
@@ -36,12 +34,15 @@ class OrderDetailsController < ApplicationController
   end
 
   def destroy
-    @detail = OrderDetail.find(params[:id])
     @detail.destroy
     redirect_to order_details_url
   end
 
   private
+
+    def set_detail
+      @detail = OrderDetail.find(params[:id])
+    end
 
     def order_details_params
       params.require(:order_detail).permit(:order_id, :product_id, :count)
