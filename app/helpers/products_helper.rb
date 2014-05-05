d93fc702b6de64fcdd8a1101f44574e3318237b1
Wataru MIYAGUNI <gonngo@gmail.com>
2014-05-05 15:45:31 +0900

    カートに追加済みの商品は、ボタンを無効化にする

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,14 @@
 module ProductsHelper
+  def add_to_cart_button(product_id)
+    options = { class: 'btn btn-default' }
+    value = 'カートに追加する'
+
+    if !session[:cart].nil? && session[:cart].include?(product_id)
+      options[:class] += ' disabled'
+      options[:disabled] = true
+      value = 'この商品は追加済みです'
+    end
+
+    button_to value, add_to_cart_path(product_id), options
+  end
 end
