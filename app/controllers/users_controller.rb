be7221ce051e2810531617d408a94210f552f048
Wataru MIYAGUNI <gonngo@gmail.com>
2014-05-05 22:52:16 +0900

    演習: トップページに、ユーザ登録直後のみ「ユーザ登録が完了しました」と表示せよ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      flash[:success] = 'ユーザ登録が完了しました'
       redirect_to root_path
     else
       render action: 'new'
